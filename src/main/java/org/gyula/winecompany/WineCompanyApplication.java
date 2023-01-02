d759c67222e915ab7f0584fcc23beb1b1a70ce2a
Gyula Szabo <44570623+gyuvi02@users.noreply.github.com>
2023-01-02 16:08:35 +0100

    Initial commit

---
+++ src/main/java/org/gyula/winecompany/WineCompanyApplication.java
@@ -0,0 +1,13 @@
+package org.gyula.winecompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineCompanyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gyula/winecompany/WineCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gyula.winecompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineCompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
